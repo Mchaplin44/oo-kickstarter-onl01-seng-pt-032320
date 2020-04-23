d3b1954232cedc963af1e8ce47a3fadea2486a79
Martin Chaplin <martin.chaplin44@gmail.com>
2020-04-23 02:34:05 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project 
+  attr_accessor :backers
+  attr_reader :title
+  
+  def intialize(title)
+    @title = title
+    @backers = []
+  end 
+
+  def add_bcker(backer)
+  @backers << backers
+  end 
+end 
